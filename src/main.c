b565db57fb15a51c5520145685823c74d72f500b
LucasTeichmann <lucteich@gmail.com>
2026-04-18 14:08:14 -0400

    testing works

--- src/main.c
+++ src/main.c
@@ -4,29 +4,38 @@
 #include <math.h>
 
 int main(){
-    int n = 2;
-    Body bodies[2];
+	int n = 2;
+	Body bodies[2];
 
-    bodies[0].mass = 1000.0;
-    bodies[0].position = (Vector2){0.0, 0.0};
-    bodies[0].velocity = (Vector2){0.0, 0.0};
+	bodies[0].mass = 1000.0;
+	bodies[0].position = (Vector2){0.0, 0.0};
+	bodies[0].velocity = (Vector2){0.0, 0.0};
 
-    bodies[1].mass = 1.0;
-    bodies[1].position = (Vector2){10.0, 0.0};
+	bodies[1].mass = 1.0;
+    	bodies[1].position = (Vector2){10.0, 0.0};
 
-    double r = 10.0;
-    double v = sqrt(G * bodies[0].mass / r);
+	double r = 10.0;
+	double v = sqrt(G * bodies[0].mass / r);
 
-    bodies[1].velocity = (Vector2){0.0, v};
+	bodies[1].velocity = (Vector2){0.0, v};
 
-    double dt = 0.001;   
-    int steps = 10000;
+	double dt = 0.001;   
+	int steps = 10000;
 
-    for(int i = 0; i < steps; i++){
-        time_step(bodies, n, dt);
+	for(int i = 0; i < steps; i++){
+		FILE *f = fopen("output.txt", "w");
 
-        printf("%f %f\n", bodies[1].position.x, bodies[1].position.y);
-    }
+		for(int i = 0; i < steps; i++){
+		time_step(bodies, n, dt);
+	
+			for(int j = 0; j < n; j++){
+			fprintf(f, "%f %f ", bodies[j].position.x, bodies[j].position.y);
+			}
+    		fprintf(f, "\n");
+		}
+
+	fclose(f);
+    	}
 
     return 0;
 }
