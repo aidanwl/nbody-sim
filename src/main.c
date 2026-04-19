5c69df0d2b0f506cf10b9e4c0eefbd4ed34eea77
awong50 <aidanwonglam@gmail.com>
2026-04-19 01:19:04 -0400

    Created 3 body problem and demonstrated semi correct orbiting using C graphics

--- src/main.c
+++ src/main.c
@@ -1,41 +1,53 @@
 #include "raylib.h"
 
-#include "math/vec2.h"
 #include "core/body.h"
 #include "physics/simulation.h"
 #include "render/renderer.h"
+#include "config/constants.h"
+#include <math.h>
 
 int main(void) {
-
-    // --- Window setup ---
     const int screenWidth = 800;
     const int screenHeight = 600;
 
-    InitWindow(screenWidth, screenHeight, "Physics Sim");
+    InitWindow(screenWidth, screenHeight, "NBody Sim");
     SetTargetFPS(60);
 
-    // --- Simulation setup ---
-    Body bodies[1];
+    Body bodies[3];
 
-    bodies[0].mass = 10.0;
-    bodies[0].position = (Vector2){400.0, 300.0};
-    bodies[0].velocity = (Vector2){50.0, 0.0};
+    // Central body
+    bodies[0].mass = 1000.0;
+    bodies[0].position = (Vector2){400.0f, 300.0f};
+    bodies[0].velocity = (Vector2){0.0f, 0.0f};
+    bodies[0].force = (Vector2){0.0f, 0.0f};
 
-    float dt = 1.0 / 60.0;
+    // Orbiting body
+    bodies[1].mass = 10.0;
+    bodies[1].position = (Vector2){414.0f, 300.0f};
+    bodies[1].velocity = (Vector2){0.0f, sqrt(G * bodies[0].mass / 10.0)};
+    bodies[1].force = (Vector2){0.0f, 0.0f};
+
+	// Orbiting body 2
+    bodies[2].mass = 1.0;
+    bodies[2].position = (Vector2){490.0f, 300.0f};
+    bodies[2].velocity = (Vector2){0.0f, sqrt(G * bodies[1].mass / 10.0)};
+    bodies[2].force = (Vector2){0.0f, 0.0f};
+
+    double dt = 1.0 / 40.0;
 
     while (!WindowShouldClose()) {
-
-        time_step(bodies, 1, dt);
+        time_step(bodies, 3, dt);
 
         BeginDrawing();
-        ClearBackground(BLACK);
+        ClearBackground(BLACK);	
 
         render_body(bodies[0].position);
+        render_body(bodies[1].position);
+		render_body(bodies[2].position);
 
         EndDrawing();
     }
 
     CloseWindow();
-
     return 0;
 }
